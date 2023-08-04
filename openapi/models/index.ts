85b372c595ec9b9523e58ac60da9e2710117adfc
KY2001 <yukiyoshikein@gmail.com>
2023-08-05 07:45:30 +0900

    add: openapi-generator

---
+++ openapi/models/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ErrorResponse';
+export * from './GetHealthAuthResponse';
+export * from './GetHealthResponse';
+export * from './GetPingResponse';
+export * from './GetUserResponse';
